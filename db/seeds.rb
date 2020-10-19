bd24b2e3410039501e417c6994b132dbf8c3320e
Ken Ingram <ken.ingram@gmail.com>
2020-10-19 15:40:04 -0700

    Create user model, controller, route using 'rails g scaffold User'; configure user password/validation/token process

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(username: 'kingram', email: 'ken.ingram@gmail.com', password_digest: '123456')
+User.create!(username: 'raymond', email: 'ken.ingram@gmail.com', password_digest: '123456')
+User.create!(username: 'david', email: 'ken.ingram@gmail.com', password_digest: '123456')
+User.create!(username: 'chris', email: 'ken.ingram@gmail.com', password_digest: '123456')
+User.create!(username: 'mary', email: 'ken.ingram@gmail.com', password_digest: '123456')
