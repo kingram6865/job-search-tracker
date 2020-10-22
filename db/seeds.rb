8e65bee65fe990c9ab85eb6a7b17f6fb98864fd7
Ken Ingram <ken.ingram@gmail.com>
2020-10-22 14:32:05 -0700

    Updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+ActivityLog.destroy_all
+Job.destroy_all
 User.destroy_all
 Company.destroy_all
 
@@ -16,11 +18,17 @@ user4 = User.create!(username: 'chris', email: 'chris@people.net', password: '12
 user5 = User.create!(username: 'bill', email: 'bill@people.net', password: '123456')
 user6 = User.create!(username: 'hal', email: 'hal@people.net', password: '123456')
 
+puts("Created #{User.count} Users")
+pp User.all
+
 company1 = Company.create(company_name:"IBM", industry:"tech", general_rating:5, external_recruiter: false)
 company2 = Company.create(company_name:"Hewlett-Packard", industry:"tech", general_rating:5, external_recruiter: false)
 company3 = Company.create(company_name:"Google", industry:"tech", general_rating:5, external_recruiter: false)
 company4 = Company.create(company_name:"Autodesk", industry:"tech", general_rating:5, external_recruiter: false)
 company5 = Company.create!(company_name:"Salesforce", industry:"tech", general_rating:5, external_recruiter: false)
+
+puts("Created #{Company.count} Companies")
+pp Company.all
 
 job1 = Job.create!(job_name:"React Developer" , keywords:"java, React", job_details:"A lot of stuff", company_id: company1.id)
 job2 = Job.create!(job_name:"Site Reliability Engineer" , keywords:"java, React", job_details:"A lot of stuff", company_id: company2.id)
@@ -29,8 +37,14 @@ job4 = Job.create!(job_name:"Web Developer" , keywords:"java, React", job_detail
 job5 = Job.create!(job_name:"Web Developer" , keywords:"java, React", job_details:"A lot of stuff", company_id: company3.id)
 job6 = Job.create!(job_name:"Web Developer" , keywords:"java, React", job_details:"A lot of stuff", company_id: company2.id)
 
+puts("Created #{Job.count} Jobs")
+pp Job.all
+
 ActivityLog.create!(entry_date: "2020-10-20 18:43:28", action:"Applied", status:"good", follow_up:"none", job_id: job1.id, user_id: user1.id)
 ActivityLog.create!(entry_date: "2020-10-20 18:43:28", action:"Declined", status:"good", follow_up:"none", job_id: job1.id, user_id: user1.id)
 ActivityLog.create!(entry_date: "2020-10-20 18:43:28", action:"Applied", status:"good", follow_up:"none", job_id: job2.id, user_id: user2.id)
 ActivityLog.create!(entry_date: "2020-10-20 18:43:28", action:"Declined", status:"good", follow_up:"none", job_id: job2.id, user_id: user2.id)
 ActivityLog.create!(entry_date: "2020-10-20 18:43:28", action:"Applied", status:"good", follow_up:"none", job_id: job3.id, user_id: user2.id)
+
+puts("Created #{ActivityLog.count} Activity Log entries")
+pp ActivityLog.all
