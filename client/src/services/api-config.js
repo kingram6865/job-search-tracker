3da9a8f7207774b9b2ed83c9931c0714699ec998
Ken Ingram <ken.ingram@gmail.com>
2020-10-22 12:36:38 -0700

    Updated api-config with production url; updated seed.rb

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://192.168.1.19:3000'
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://jobsearchtracker-api.herokuapp.com' : 'http://192.168.1.19:3000'
 
 const api = axios.create({
   baseURL: baseUrl
