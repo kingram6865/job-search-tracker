de8e19299eed80db7006877a1f03feb72678c325
Ken Ingram <ken.ingram@gmail.com>
2020-10-23 15:46:08 -0700

    Some final coding touches to Companies context

--- client/src/components/CompanyDetail/CompanyDetail.jsx
+++ client/src/components/CompanyDetail/CompanyDetail.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { Link, useParams, useHistory } from 'react-router-dom'
 import { getOneCompany } from '../../services/company'
 import { verifyUser } from '../../services/auth'
 
 export default function Company(props) {
   const [company, setCompany] = useState(null)
   const { id } = useParams()
+  const history = useHistory()
 
   useEffect(() => {
     verifyUser()
@@ -27,7 +28,11 @@ export default function Company(props) {
           <p>{company.general_rating}</p>
           {company.external_recruiter ? (<p>Recruiter</p>) : null}
           {/* {company.jobs.forEach((job) => (<p>{job}</p>))} */}
-          <Link to='/companies'>Return to Company List</Link>
+          <li><Link to='/companies'>Return to Company List</Link></li>
+          <button onClick={() => history.push(`/companies/${id}/edit`)}>Edit</button>
+          <button onClick={()=> alert(`Deleting Company ${id}`)}>Delete</button>
+
+
         </>
       }
     </div>
